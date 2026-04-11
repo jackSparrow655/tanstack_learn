c77d79a456bd5ef9db5229213ecf7c60a1b31f4e
ARIJIT BARIK <arijitbarik655@gmail.com>
2026-04-11 18:41:05 +0530

    delete implemented

--- src/routes/(learn)/folder-structure/-lib/store/folderDataStore.ts
+++ src/routes/(learn)/folder-structure/-lib/store/folderDataStore.ts
@@ -2,18 +2,22 @@ import { create } from 'zustand'
 import type { ItemType } from '../type'
 import data from '../data.json'
 
-export type SelectedFolderType = {
+export type SelectedFolderTypeForAdd = {
   id: string
   fileType: 'file' | 'folder'
+}
+export type SelectedFolderTypeForEditDelete = {
+  id: string
+  action: 'edit' | 'delete'
 }
 
 type State = {
   folderStructureData: ItemType
   updateFolderStructureData: (data: ItemType) => void
-  selectedFolder: SelectedFolderType | null
-  setSelectedFolder: (data: SelectedFolderType | null) => void
-  selectedIdForEditDelete: string | null
-  setSelectedIdForEditDelete: (id: string | null) => void
+  selectedFolderForAdd: SelectedFolderTypeForAdd | null
+  setSelectedFolderForAdd: (data: SelectedFolderTypeForAdd | null) => void
+  selectedIdForEditDelete: SelectedFolderTypeForEditDelete | null
+  setSelectedIdForEditDelete: (data: SelectedFolderTypeForEditDelete | null) => void
 }
 
 // Create your store, which includes both state and (optionally) actions
@@ -21,8 +25,8 @@ export const useFolderStructureData = create<State>((set) => ({
   folderStructureData: data as ItemType,
   updateFolderStructureData: (updatedData: ItemType) =>
     set({ folderStructureData: updatedData }),
-  selectedFolder: null,
-  setSelectedFolder: (data) => set({ selectedFolder: data }),
+  selectedFolderForAdd: null,
+  setSelectedFolderForAdd: (data) => set({ selectedFolderForAdd: data }),
   selectedIdForEditDelete: null,
-  setSelectedIdForEditDelete: (id) => set({ selectedIdForEditDelete: id }),
+  setSelectedIdForEditDelete: (data) => set({ selectedIdForEditDelete: data }),
 }))
--- src/routes/(learn)/folder-structure/index.tsx
+++ src/routes/(learn)/folder-structure/index.tsx
@@ -3,16 +3,27 @@ import type { FileType, FolderType, Item, ItemType } from './-lib/type'
 import { ChevronDown, ChevronRightIcon, File, Folder } from 'lucide-react'
 import {
   useFolderStructureData,
-  type SelectedFolderType,
+  type SelectedFolderTypeForAdd,
 } from './-lib/store/folderDataStore'
 import { Input } from '@/components/ui/input'
-import { useState } from 'react'
+import { useState, type SetStateAction } from 'react'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
 
 export const Route = createFileRoute('/(learn)/folder-structure/')({
   component: RouteComponent,
 })
 
-const EditFileFolderInput = ({ value }: { value: string }) => {
+const EditFileFolderInput = ({
+  value,
+  setEditDeleteModalOpen,
+}: {
+  value: string
+  setEditDeleteModalOpen: (value: SetStateAction<boolean>) => void
+}) => {
   const [inputVal, setInputVal] = useState(value)
   const handleInputChange = (e: any) => {
     setInputVal(e.target.value)
@@ -27,21 +38,24 @@ const EditFileFolderInput = ({ value }: { value: string }) => {
   const handleKeyDown = (e: any) => {
     if (e.nativeEvent.key === 'Enter') {
       const itemIndexArr =
-        selectedIdForEditDelete?.split('.').map((item) => Number(item)) ?? []
+        selectedIdForEditDelete?.id?.split('.').map((item) => Number(item)) ??
+        []
       const numberOfIndex = itemIndexArr?.length ?? 0
       const startFolder = copyData[itemIndexArr[0]] as FolderType
-      const updatedFolder = handleAddEditFileFolder(
+      const updatedFolder = handleAddEditDeleteFileFolder(
         startFolder,
         itemIndexArr,
         numberOfIndex,
         0,
-        inputVal,
         'edit',
+        inputVal,
       )
-      copyData[itemIndexArr[0]] = updatedFolder
+      if (updatedFolder) {
+        copyData[itemIndexArr[0]] = updatedFolder
+      }
       updateFolderStructureData(copyData)
-      console.log('updated = ', copyData)
       setSelectedIdForEditDelete(null)
+      setEditDeleteModalOpen(false)
     } else return
   }
   return (
@@ -56,19 +70,74 @@ const EditFileFolderInput = ({ value }: { value: string }) => {
 }
 
 const ItemComponent = ({ item }: { item: Item }) => {
-  const { setSelectedIdForEditDelete, selectedIdForEditDelete } =
-    useFolderStructureData()
+  const [editDeleteModalOpen, setEditDeleteModalOpen] = useState<boolean>(false)
+  const {
+    setSelectedIdForEditDelete,
+    selectedIdForEditDelete,
+    folderStructureData,
+    updateFolderStructureData,
+  } = useFolderStructureData()
 
-  const handleRightClick = (e: React.MouseEvent<HTMLDivElement>) => {
+  const handleRightClick = (e: React.MouseEvent) => {
     e.preventDefault()
-    setSelectedIdForEditDelete(item.id)
+    setEditDeleteModalOpen(true)
+  }
+  const handleDelete = (id: string) => {
+    setSelectedIdForEditDelete({ id: id, action: 'delete' })
+    let copyData = structuredClone(folderStructureData)
+    const itemIndexArr = id?.split('.').map((item) => Number(item)) ?? []
+    const numberOfIndex = itemIndexArr?.length ?? 0
+    const startFolder = copyData[itemIndexArr[0]] as FolderType
+    const updatedFolder = handleAddEditDeleteFileFolder(
+      startFolder,
+      itemIndexArr,
+      numberOfIndex,
+      0,
+      'delete',
+      undefined,
+      undefined,
+      id,
+    )
+    if (updatedFolder) {
+      copyData[itemIndexArr[0]] = updatedFolder
+    }
+    updateFolderStructureData(copyData)
+    setEditDeleteModalOpen(false)
   }
   return (
     <>
-      {selectedIdForEditDelete === item.id ? (
-        <EditFileFolderInput value={item.name} />
+      {selectedIdForEditDelete?.action === 'edit' &&
+      selectedIdForEditDelete?.id === item.id ? (
+        <EditFileFolderInput
+          value={item.name}
+          setEditDeleteModalOpen={setEditDeleteModalOpen}
+        />
       ) : (
-        <span onContextMenu={handleRightClick}>{item.name}</span>
+        <Popover
+          open={editDeleteModalOpen}
+          onOpenChange={(e) => {
+            if (e && !editDeleteModalOpen) return
+            setEditDeleteModalOpen(e)
+          }}
+        >
+          <PopoverTrigger asChild>
+            <span onContextMenu={handleRightClick}>{item.name}</span>
+          </PopoverTrigger>
+          <PopoverContent
+            side="right"
+            onInteractOutside={() => setEditDeleteModalOpen(false)}
+            className="flex flex-col p-1 w-fit"
+          >
+            <span
+              onClick={() =>
+                setSelectedIdForEditDelete({ id: item.id, action: 'edit' })
+              }
+            >
+              Edit name
+            </span>
+            <span onClick={() => handleDelete(item.id)}>Delete</span>
+          </PopoverContent>
+        </Popover>
       )}
     </>
   )
@@ -121,14 +190,13 @@ const ChevronIconComponnent = ({ item }: { item: FolderType }) => {
 }
 
 const AddFileFolderComponent = ({ item }: { item: FolderType }) => {
-  const { setSelectedFolder } = useFolderStructureData()
+  const { setSelectedFolderForAdd } = useFolderStructureData()
   const handleSelect = (type: 'file' | 'folder') => {
-    const data: SelectedFolderType = {
+    const data: SelectedFolderTypeForAdd = {
       id: item.id,
       fileType: type,
     }
-    console.log('data = ', data)
-    setSelectedFolder(data)
+    setSelectedFolderForAdd(data)
   }
   return (
     <div className="flex items-center gap-1 mt-0.5">
@@ -146,15 +214,53 @@ const AddFileFolderComponent = ({ item }: { item: FolderType }) => {
   )
 }
 
-const handleAddEditFileFolder = (
+const refactorId = (
+  item: (FolderType | FileType)[],
+  criticalIndexVal: number,
+  indexPosition: number,
+): (FolderType | FileType)[] => {
+  console.log(
+    'item, criticalIndexVal, indexPosition = ',
+    item,
+    criticalIndexVal,
+    indexPosition,
+  )
+  if (item.length === 0) {
+    return item
+  }
+  for (let i = 0; i < item.length; i++) {
+    const updatedId = item[i].id
+      .split('.')
+      .map((el, i) => {
+        if (i === indexPosition && Number(el) >= criticalIndexVal) {
+          return Number(el) - 1
+        } else {
+          return Number(el)
+        }
+      })
+      .join('.')
+    item[i].id = updatedId
+    if (item[i].isFolder) {
+      refactorId(
+        (item[i] as FolderType)?.items,
+        criticalIndexVal,
+        indexPosition,
+      )
+    }
+  }
+  return item
+}
+
+const handleAddEditDeleteFileFolder = (
   item: FolderType,
   indexArr: number[],
   numberOfIndex: number,
   currIndex: number,
-  inputVal: string,
-  action: 'edit' | 'add',
+  action: 'edit' | 'add' | 'delete',
+  inputVal?: string,
   fileType?: 'folder' | 'file',
-): FolderType => {
+  deleteItemId?: string,
+): FolderType | null => {
   if (currIndex === numberOfIndex - 1) {
     if (action === 'add') {
       let newId =
@@ -163,37 +269,53 @@ const handleAddEditFileFolder = (
       if (fileType === 'file') {
         newFileOrFolder = {
           id: newId,
-          name: inputVal,
+          name: inputVal ?? '',
           isFolder: false,
         }
       } else {
         newFileOrFolder = {
           id: newId,
           isFolder: true,
-          name: inputVal,
+          name: inputVal ?? '',
           isOpen: true,
           items: [],
         }
       }
-      console.log('item.items.push', item)
       item.items.push(newFileOrFolder)
+    } else if (action === 'edit') {
+      item.name = inputVal ?? ''
     } else {
-      item.name = inputVal
+      console.log('item to delete = ', item)
+      return null
     }
     return item
   }
   const nextIndex = indexArr[currIndex + 1]
   let currItem = item?.items[nextIndex] as FolderType
-  const updatedItem = handleAddEditFileFolder(
+  const updatedItem = handleAddEditDeleteFileFolder(
     currItem,
     indexArr,
     numberOfIndex,
     currIndex + 1,
-    inputVal,
     action,
+    inputVal,
     fileType,
+    deleteItemId,
   )
-  item.items[nextIndex] = updatedItem
+  if (updatedItem) {
+    item.items[nextIndex] = updatedItem
+  } else {
+    // console.log('nextIndex = ', nextIndex)
+    // console.log('item.items[nextIndex] = ', item.items[nextIndex])
+    const ItemsToRefactor = item.items.filter((item) => {
+      console.log(item.id, deleteItemId)
+      return item.id !== deleteItemId
+    })
+    const updatedItems = refactorId(ItemsToRefactor, nextIndex, currIndex + 1)
+    item.items = updatedItems
+    console.log('updated Items = ', updatedItems)
+    // item.items
+  }
   return item
 }
 const AddFileFolderInput = () => {
@@ -204,29 +326,30 @@ const AddFileFolderInput = () => {
   const {
     updateFolderStructureData,
     folderStructureData,
-    selectedFolder,
-    setSelectedFolder,
+    selectedFolderForAdd,
+    setSelectedFolderForAdd,
   } = useFolderStructureData()
   let copyData = structuredClone(folderStructureData)
   const handleKeyDown = (e: any) => {
     if (e.nativeEvent.key === 'Enter') {
       const itemIndexArr =
-        selectedFolder?.id.split('.').map((item) => Number(item)) ?? []
+        selectedFolderForAdd?.id.split('.').map((item) => Number(item)) ?? []
       const numberOfIndex = itemIndexArr?.length ?? 0
       const startFolder = copyData[itemIndexArr[0]] as FolderType
-      const updatedFolder = handleAddEditFileFolder(
+      const updatedFolder = handleAddEditDeleteFileFolder(
         startFolder,
         itemIndexArr,
         numberOfIndex,
         0,
-        inputVal,
         'add',
-        selectedFolder?.fileType as 'file' | 'folder',
+        inputVal,
+        selectedFolderForAdd?.fileType as 'file' | 'folder',
       )
-      copyData[itemIndexArr[0]] = updatedFolder
+      if (updatedFolder) {
+        copyData[itemIndexArr[0]] = updatedFolder
+      }
       updateFolderStructureData(copyData)
-      console.log('updated = ', copyData)
-      setSelectedFolder(null)
+      setSelectedFolderForAdd(null)
     } else return
   }
   return (
@@ -244,23 +367,23 @@ const RenderItems = ({ item }: { item: ItemType }) => {
   if (item.length === 0) {
     return null
   }
-  const { selectedFolder } = useFolderStructureData()
-  console.log('selected folder = ', selectedFolder)
-  // const sortedItem = item.sort((a, b) => {
-  //   // 1. Sort by fileType: "folder" before "file"
-  //   if (a.isFolder !== b.isFolder) {
-  //     return a.isFolder ? -1 : 1
-  //   }
+  const { selectedFolderForAdd } = useFolderStructureData()
+  // console.log('selected folder = ', selectedFolderForAdd)
+  const sortedItem = item.sort((a, b) => {
+    // 1. Sort by fileType: "folder" before "file"
+    if (a.isFolder !== b.isFolder) {
+      return a.isFolder ? -1 : 1
+    }
 
-  //   // 2. If types are the same, sort by fileName alphabetically
-  //   return a.name.localeCompare(b.name, undefined, {
-  //     numeric: true, // Sorts numbers naturally (e.g., "file2" before "file10")
-  //     sensitivity: 'base', // Ignores case and accents
-  //   })
-  // })
+    // 2. If types are the same, sort by fileName alphabetically
+    return a.name.localeCompare(b.name, undefined, {
+      numeric: true, // Sorts numbers naturally (e.g., "file2" before "file10")
+      sensitivity: 'base', // Ignores case and accents
+    })
+  })
   return (
     <div>
-      {item.map((el) => {
+      {sortedItem.map((el) => {
         switch (el.isFolder) {
           //folder
           case true: {
@@ -276,7 +399,7 @@ const RenderItems = ({ item }: { item: ItemType }) => {
                     <RenderItems item={el.items} />
                   </div>
                 )}
-                {el.id === selectedFolder?.id && <AddFileFolderInput />}
+                {el.id === selectedFolderForAdd?.id && <AddFileFolderInput />}
               </div>
             )
           }
@@ -296,6 +419,7 @@ const RenderItems = ({ item }: { item: ItemType }) => {
 
 function RouteComponent() {
   const { folderStructureData } = useFolderStructureData()
+  console.log('folder structure data = ', folderStructureData)
   return (
     <div className="flex-1">
       <div className="max-w-xs border h-full">
@@ -304,3 +428,34 @@ function RouteComponent() {
     </div>
   )
 }
+
+// [
+//   {
+//     "id": "0",
+//     "name": "route",
+//     "isFolder": true,
+//     "items": [
+//       {
+//         "id": "0.0",
+//         "name": "design-pattern",
+//         "isFolder": true,
+//         "items": [],
+//         "isOpen": true
+//       },
+//       {
+//         "id": "0.1",
+//         "name": "file manager",
+//         "isFolder": true,
+//         "items": [
+//           {
+//             "id": "0.1.0",
+//             "name": "index.tsx",
+//             "isFolder": false
+//           }
+//         ],
+//         "isOpen": true
+//       }
+//     ],
+//     "isOpen": true
+//   }
+// ]
